11051775da23246ff8cfe60eaaa5d6e73d39e00a
Silvio Nunes <silvioinco@yahoo.com>
2009-12-06 21:22:53 -0700

    Signed-off-by: Silvio Nunes <silvioinco@yahoo.com>

--- src/com/coolisland/client/controller/LoginController.java
+++ src/com/coolisland/client/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.coolisland.client.controller;
 
 import com.allen_sauer.gwt.log.client.Log;
+import com.coolisland.client.GreetingService;
+import com.coolisland.client.GreetingServiceAsync;
 import com.coolisland.client.view.LoginView;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * Handler for the sendButton and nameField. This class is responsible for
@@ -11,13 +15,28 @@ import com.coolisland.client.view.LoginView;
  * 
  */
 public class LoginController {
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
+	 */
+	private final GreetingServiceAsync	greetingService	   = GWT.create(GreetingService.class);
+
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String	       SERVER_ERROR	       = "An error occurred while "
+	                                                               + "attempting to contact the server. Please check your network "
+	                                                               + "connection and try again.";
+	private boolean	                   initialized	       = false;
+	private static String	           logDebugIndentation	= "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
 
 	private LoginController() {
-		Log.debug("Constructor MathController() starting");
+		Log.debug(logDebugIndentation + "Constructor MathController() starting");
 
 		initialize();
 
-		Log.debug("Constructor MathController() finished");
+		Log.debug(logDebugIndentation + "Constructor MathController() finished");
 	}
 
 	/**
@@ -30,7 +49,7 @@ public class LoginController {
 	}
 
 	public static LoginController getInstance() {
-		Log.debug("LoginController.getInstance() starting");
+		Log.debug(logDebugIndentation + "LoginController.getInstance()");
 
 		return SingletonHolder.theLoginControllerSingletonInst;
 	}
@@ -39,6 +58,67 @@ public class LoginController {
 	 * Initializes the login
 	 */
 	public void initialize() {
+		Log.debug(logDebugIndentation + this.getClass().getName() + " initialize() starting");
+
+		if (initialized) {
+			Log.debug(logDebugIndentation + this.getClass().getName()
+			        + " initialize() we've already initialized");
+			return;
+		}
+
 		LoginView.getInstance().initialize();
+		initialized = true;
+
+		Log.debug(logDebugIndentation + this.getClass().getName() + " initialize() finished");
 	}
+
+	/**
+	 * Send the name from the nameField to the server and wait for a response.
+	 */
+	public void sendNameToServer(String loginName) {
+		Log.debug(logDebugIndentation + this.getClass().getName() + " sendNameToServer() starting");
+
+		// disable the send button from the login screen
+		LoginView.getInstance().disableSend();
+
+		/*
+		 * submit the login name to the server and pop-up a screen indicating if
+		 * we had success or failure logging in
+		 */
+		greetingService.greetServer(loginName, new AsyncCallback<String>() {
+			/**
+			 * onFailure will be called if the login sent to the server fails
+			 */
+			public void onFailure(Throwable caught) {
+				Log.debug(logDebugIndentation + this.getClass().getName()
+				        + " greetServer.onFailure() starting");
+
+				LoginView.getInstance().displayLoginResponse(false, SERVER_ERROR);
+
+				Log.debug(logDebugIndentation + this.getClass().getName()
+				        + " greetServer.onFailure() finished");
+			}
+
+			/**
+			 * onSuccess will be called if the login sent to the server succeeds
+			 * 
+			 * @param result
+			 */
+			public void onSuccess(String result) {
+				Log.debug(logDebugIndentation + this.getClass().getName()
+				        + " greetServer.onSuccess() starting");
+
+				LoginView.getInstance().displayLoginResponse(true, result);
+
+				// startup the math application
+				MathController.getInstance().initialize();
+
+				Log.debug(logDebugIndentation + this.getClass().getName()
+				        + " greetServer.onSuccess() finished");
+			}
+		});
+
+		Log.debug(logDebugIndentation + this.getClass().getName() + " sendNameToServer() finished");
+	}
+
 }
--- src/com/coolisland/client/view/LoginView.java
+++ src/com/coolisland/client/view/LoginView.java
@@ -1,50 +1,32 @@
 package com.coolisland.client.view;
 
 import com.allen_sauer.gwt.log.client.Log;
-import com.coolisland.client.GreetingService;
-import com.coolisland.client.GreetingServiceAsync;
-import com.coolisland.client.controller.MathController;
+import com.coolisland.client.controller.LoginController;
 import com.coolisland.client.model.LoginModel;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
-public class LoginView implements ClickHandler, KeyUpHandler {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String	       SERVER_ERROR	       = "An error occurred while "
-	                                                               + "attempting to contact the server. Please check your network "
-	                                                               + "connection and try again.";
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting
-	 * service.
-	 */
-	private final GreetingServiceAsync	greetingService	   = GWT.create(GreetingService.class);
+public class LoginView {
+	DialogBox	          dialogBox	            = null;
+	Button	              closeButton	        = null;
+	// Label textToServerLabel = null;
+	HTML	              serverResponseLabel	= null;
+	final Button	      sendButton	        = new Button("Send");
+	final TextBox	      nameField	            = new TextBox();
 
-	DialogBox	                       dialogBox	       = null;
-	Button	                           closeButton	       = null;
-	Label	                           textToServerLabel	= null;
-	HTML	                           serverResponseLabel	= null;
-	final Button	                   sendButton	       = new Button("Send");
-	final TextBox	                   nameField	       = new TextBox();
-
-	private static String	           logDebugIndentation	= "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
-	private final boolean	           initialized	       = false;
+	private static String	logDebugIndentation	= "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+	private boolean	      initialized	        = false;
 
 	private LoginView() {
-		Log.debug(logDebugIndentation + "Constructor LoginView()");
+		Log.debug(logDebugIndentation + "Constructor LoginView() starting");
 
 		initialize();
 
@@ -67,9 +49,92 @@ public class LoginView implements ClickHandler, KeyUpHandler {
 	}
 
 	/**
+	 * Handler for the sendButton and nameField. This class is responsible for
+	 * handling the user logging in.
+	 * 
+	 * @author Silvio
+	 * 
+	 */
+	class LoginHandler implements ClickHandler, KeyUpHandler {
+		DialogBox	dialogBox		    = null;
+		Button		closeButton		    = null;
+		// Label textToServerLabel = null;
+		HTML		serverResponseLabel	= null;
+
+		public LoginHandler() {
+			// Create the popup dialog box
+			dialogBox = new DialogBox();
+			dialogBox.setText("Remote Procedure Call");
+			dialogBox.setAnimationEnabled(true);
+
+			closeButton = new Button("Close");
+			// textToServerLabel = new Label();
+			serverResponseLabel = new HTML();
+		}
+
+		/**
+		 * Fired when the user clicks on the send button to login
+		 */
+		public void onClick(ClickEvent event) {
+			// get the login name the user entered
+			String loginName = LoginModel.getInstance().getLoginName();
+
+			LoginController.getInstance().sendNameToServer(loginName);
+		}
+
+		/**
+		 * Fired when the user types in the nameField.
+		 */
+		public void onKeyUp(KeyUpEvent event) {
+			if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+				// get the login name the user entered
+				String loginName = LoginModel.getInstance().getLoginName();
+
+				LoginController.getInstance().sendNameToServer(loginName);
+			}
+		}
+	}
+
+	/**
+	 * display server response
+	 */
+	public void displayLoginResponse(boolean sucess, String result) {
+
+		if (!sucess) {
+			// Show the RPC error message to the user
+			dialogBox.setText("Remote Procedure Call - Failure");
+			serverResponseLabel.addStyleName("serverResponseLabelError");
+			serverResponseLabel.setHTML(result);
+			dialogBox.center();
+			closeButton.setFocus(true);
+		} else {
+			// don't display the success message
+			// dialogBox.setText("Remote Procedure Call");
+			// serverResponseLabel.removeStyleName("serverResponseLabelError");
+			// serverResponseLabel.setHTML(result);
+			// dialogBox.center();
+			// closeButton.setFocus(true);
+		}
+
+		// Add a handler to close the DialogBox
+		closeButton.addClickHandler(new ClickHandler() {
+			/**
+			 * Handles the close button click from the RPC pop-up
+			 */
+			public void onClick(ClickEvent event) {
+				dialogBox.hide();
+				sendButton.setEnabled(true);
+				sendButton.setFocus(true);
+			}
+		});
+	}
+
+	/**
 	 * initializes the navigation panel with some hard coded strings
 	 */
 	public void initialize() {
+		Log.debug(logDebugIndentation + this.getClass().getName() + ".initialize() starting");
+
 		// if we have already initialized, just return
 		if (initialized) {
 			Log.debug(logDebugIndentation + this.getClass().getName()
@@ -82,7 +147,7 @@ public class LoginView implements ClickHandler, KeyUpHandler {
 		dialogBox.setAnimationEnabled(true);
 
 		closeButton = new Button("Close");
-		textToServerLabel = new Label();
+		// textToServerLabel = new Label();
 		serverResponseLabel = new HTML();
 
 		nameField.setText("William");
@@ -99,125 +164,37 @@ public class LoginView implements ClickHandler, KeyUpHandler {
 		nameField.setFocus(true);
 		nameField.selectAll();
 
-		Log.debug(logDebugIndentation + this.getClass().getName() + ".initialize() starting");
+		// Add a handler to send the name to the server
+		LoginHandler handler = new LoginHandler();
+		sendButton.addClickHandler(handler);
+		nameField.addKeyUpHandler(handler);
+
+		// set flag indicating that we have initialized ourselves
+		initialized = true;
+
+		Log.debug(logDebugIndentation + this.getClass().getName() + ".initialize() finished");
 	}
 
 	/**
 	 * Disables the send button
 	 */
 	public void disableSend() {
+		Log.debug(logDebugIndentation + "disableSend() starting");
+
 		sendButton.setEnabled(false);
+
+		Log.debug(logDebugIndentation + "disableSend() finished");
 	}
 
 	/**
 	 * Disables the send button
 	 */
 	public void enableSend() {
+		Log.debug(logDebugIndentation + "enableSend() starting");
+
 		sendButton.setEnabled(true);
-	}
 
-	/**
-	 * Fired when the user clicks on the send button to login
-	 */
-	@Override
-	public void onClick(ClickEvent event) {
-		sendNameToServer();
-	}
-
-	/**
-	 * Fired when the user types in the nameField.
-	 */
-	@Override
-	public void onKeyUp(KeyUpEvent event) {
-		if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-			sendNameToServer();
-		}
-	}
-
-	/**
-	 * display server response
-	 */
-	private void displayServerResponse(String result) {
-		// don't display the success message
-		return;
-
-		// dialogBox.setText("Remote Procedure Call");
-		// serverResponseLabel.removeStyleName("serverResponseLabelError");
-		// serverResponseLabel.setHTML(result);
-		// dialogBox.center();
-		// closeButton.setFocus(true);
-
-		// // Add a handler to close the DialogBox
-		// closeButton.addClickHandler(new ClickHandler() {
-		// /**
-		// * Handles the close button click from the RPC pop-up
-		// */
-		// public void onClick(ClickEvent event) {
-		// dialogBox.hide();
-		// sendButton.setEnabled(true);
-		// sendButton.setFocus(true);
-		// }
-		// });
-		//
-		// // We can set the id of a widget by accessing its Element
-		// closeButton.getElement().setId("closeButton");
-		// VerticalPanel dialogVPanel = new VerticalPanel();
-		// dialogVPanel.addStyleName("dialogVPanel");
-		// dialogVPanel.add(new
-		// HTML("<b>Sending name to the server:</b>"));
-		// dialogVPanel.add(textToServerLabel);
-		// dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		// dialogVPanel.add(serverResponseLabel);
-		// dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		// dialogVPanel.add(closeButton);
-		// dialogBox.setWidget(dialogVPanel);
-	}
-
-	/**
-	 * Send the name from the nameField to the server and wait for a response.
-	 */
-	private void sendNameToServer() {
-		// disable the send button from the login screen
-		LoginView.getInstance().disableSend();
-
-		// get the login name the user entered
-		textToServerLabel.setText(LoginModel.getInstance().getLoginName());
-
-		String textToServer = nameField.getText();
-		textToServerLabel.setText(textToServer);
-
-		// clear the server response
-		serverResponseLabel.setText("");
-
-		/*
-		 * submit the login name to the server and pop-up a screen indicating if
-		 * we had success or failure logging in
-		 */
-		greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-			/**
-			 * onFailure will be called if the login sent to the server fails
-			 */
-			public void onFailure(Throwable caught) {
-				// Show the RPC error message to the user
-				dialogBox.setText("Remote Procedure Call - Failure");
-				serverResponseLabel.addStyleName("serverResponseLabelError");
-				serverResponseLabel.setHTML(SERVER_ERROR);
-				dialogBox.center();
-				closeButton.setFocus(true);
-			}
-
-			/**
-			 * onSuccess will be called if the login sent to the server succeeds
-			 * 
-			 * @param result
-			 */
-			public void onSuccess(String result) {
-				displayServerResponse(result);
-
-				// startup the math application
-				MathController.getInstance().initialize();
-			}
-		});
+		Log.debug(logDebugIndentation + "enableSend() finished");
 	}
 
 }
